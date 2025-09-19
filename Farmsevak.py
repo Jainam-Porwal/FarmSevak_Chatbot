237ba93e42ef42d50fa9cf0d4720d46f9d6da207
Jainam Porwal <133258501+Jainam-Porwal@users.noreply.github.com>
2025-09-19 14:55:46 +0530

    Update Farmsevak.py

--- Farmsevak.py
+++ Farmsevak.py
@@ -9,7 +9,7 @@ import os
 load_dotenv()
 
 # Load model
-llm = HuggingFaceEndpoint(repo_id="deepseek-ai/DeepSeek-V3.1")
+llm = HuggingFaceEndpoint(repo_id="openai/gpt-oss-20b")
 model = ChatHuggingFace(llm=llm)
 parser = StrOutputParser()
 
